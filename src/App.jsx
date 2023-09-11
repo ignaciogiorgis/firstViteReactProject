912cfe33e2d0823580284f59297a3fd8cb50c9fe
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2023-09-10 21:16:23 -0300

    08-view-data-shift-panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import ContainerHeader from "./Components/ContainerHeader"
-import ContainerForm from "./Components/ContainerForm"
-import ContainerShiftPanel from "./Components/ContainerShiftPanel"
+import ContainerApp from "./Components/ContainerApp"
 
 function App() {
   return (
     <>
-      <div className="text-center py-10">
-        <ContainerHeader />
-      </div>
-      <div className="flex justify-evenly ">
-        <div className="text-center">
-          <ContainerForm />
-        </div>
-        <div className="text-center">
-          <ContainerShiftPanel />
-        </div>
-      </div>
+      <ContainerApp />
     </>
   )
 }
